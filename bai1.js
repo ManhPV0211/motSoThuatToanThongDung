adcbe80beb1a65adc1af89661669ae2d8ccea394
Phan Manh <manh.phanvan.hust@gmail.com>
2022-04-05 15:23:04 +0700

    dang lam thuat toan 2

--- bai1.js
+++ bai1.js
@@ -7,3 +7,23 @@ function algorithm1(x, y){
     init[1] = tg;
     console.log(init);
 }
+// Thuat toan kiem tra so nguyen to
+function algorithm2(x){
+    let flag = true;
+    for(i = 1; i < x; i++){
+        if(x%i == 0){
+            flag = false;
+        }
+    }
+    if(flag = true){
+        console.log("Day la so nguyen to");
+    } else{
+        console.log("Day khong phai la so nguyen to");
+    }
+}
+// chua xong bai nay
+
+// Thuat toan tim phan tu lon nhat
+function algorithm3(){
+    
+}
